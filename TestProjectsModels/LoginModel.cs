36f9518786f0901b0666466c41f6b7b40361d659
shasank kittu <shasank.kittu@outlook.com>
2018-09-12 12:30:48 +0530

    fhja

--- TestProjectsModels/LoginModel.cs
+++ TestProjectsModels/LoginModel.cs
@@ -10,6 +10,8 @@ namespace TestProjectsModels
     {
         public string Email { get; set; }
         public string Password { get; set; }
+        public string ConfirmPas { get; set; }
+
 
     }
 }
