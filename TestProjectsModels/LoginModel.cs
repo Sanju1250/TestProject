36cf0825887c8f0c4a945779bb4be7b1ab52de88
sanjudombale414 <sanjudombale414@gmail.com>
2018-09-12 12:57:05 +0530

    add prop

--- TestProjectsModels/LoginModel.cs
+++ TestProjectsModels/LoginModel.cs
@@ -9,6 +9,7 @@ namespace TestProjectsModels
    public class LoginModel
     {
         public string Email { get; set; }
-        public string Password { get; set; }    
+        public string Password { get; set; }
+        public int MyProperty { get; set; }
     }
 }
