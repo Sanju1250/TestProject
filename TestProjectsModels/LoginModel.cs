18f639e471db05598116c14cb62bc30bca43dc51
sanjudombale414 <sanjudombale414@gmail.com>
2018-09-12 12:32:52 +0530

    confirm password not required

--- TestProjectsModels/LoginModel.cs
+++ TestProjectsModels/LoginModel.cs
@@ -10,8 +10,6 @@ namespace TestProjectsModels
     {
         public string Email { get; set; }
         public string Password { get; set; }
-        public string ConfirmPas { get; set; }
-
-
+       
     }
 }
